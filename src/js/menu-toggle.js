dfafc8ddc3005d1009ae572cf5f1996838371da9
zylag <tamaszilagyi@gmail.com>
2024-07-18 15:29:10 +0200

    mobile menu tweak

--- src/js/menu-toggle.js
+++ src/js/menu-toggle.js
@@ -1,10 +1,28 @@
 // menu-toggle.js
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.sandwich').addEventListener('click', function() {
-        document.querySelector('.mobile-menu').classList.toggle('show');
-        document.querySelectorAll('.hamburger--spring').forEach(function(element) {
+    const sandwich = document.querySelector('.sandwich');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const hamburgerSpring = document.querySelectorAll('.hamburger--spring');
+    const contactButton = document.getElementById('contactbutton');
+
+    function toggleMenu() {
+        mobileMenu.classList.toggle('show');
+        hamburgerSpring.forEach(function(element) {
             element.classList.toggle('is-active');
         });
-    });
+    }
+
+    function closeMenu() {
+        mobileMenu.classList.remove('show');
+        hamburgerSpring.forEach(function(element) {
+            element.classList.remove('is-active');
+        });
+    }
+
+    sandwich.addEventListener('click', toggleMenu);
+
+    if (contactButton) {
+        contactButton.addEventListener('click', closeMenu);
+    }
 });
 
