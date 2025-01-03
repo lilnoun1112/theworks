756b6f3eef411ec29db2d7a04ccea4a5b460e3e8
zylag <tamaszilagyi@gmail.com>
2025-01-03 12:01:14 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/images');
   eleventyConfig.addPassthroughCopy('./src/js');
   eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy('./src/.well-known');
   eleventyConfig.addPassthroughCopy('./static');
 
   eleventyConfig.addCollection("featured", function(collectionApi) {
